527b35cb942cb950bde698926229f4d743db8d8b
Erick <e.velasquez.tech@gmail.com>
2025-09-25 10:47:04 +0200

    Archivos que programe en el instituto

---
+++ src/main/java/org/example/PC/Excercises/whileExercises/Ej3.java
@@ -0,0 +1,11 @@
+package org.example.PC.Excercises.whileExercises;
+
+import java.util.Scanner;
+
+public class Ej3 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("");
+        while
+    }
+}
