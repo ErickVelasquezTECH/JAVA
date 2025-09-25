28d9efc595b14ed1b9f5e64f27148afa00431aa9
Erick <e.velasquez.tech@gmail.com>
2025-09-25 11:15:40 +0200

    Archivos que programe en el instituto

--- src/main/java/org/example/PC/Excercises/forExercises/Ej9.java
+++ src/main/java/org/example/PC/Excercises/forExercises/Ej9.java
@@ -6,16 +6,22 @@ public class Ej9 {
         System.out.println("Dime un numero mayor a 1");
         int numb1=scanner.nextInt();
         scanner.nextLine();
-        boolean cousin=true;
-        for (int i=2; i<numb1;i++){
-            if (numb1%i==0){
-            cousin=false;
-            };
+        boolean isprime=true;
+        if (numb1>0) {
+            for (int i = 2; i < numb1; i++) {
+                if (numb1 % i == 0) {
+                    isprime = false;
+                }
+            }
         }
-        if (cousin){
+        if (numb1<0){
+            System.out.println("No existe primo negativo");
+
+        } else if (isprime) {
             System.out.println("Es primo");
         } else {
-            System.out.println("No es primo");
-        }
+        System.out.println("No es primo");
+    }
+
     }
 }
