32e51a25dc5520777bccee600b4395436cc7d1e7
Erick <e.velasquez.tech@gmail.com>
2025-09-29 14:19:51 +0200

    Archivos que programe en el instituto

---
+++ src/main/java/org/example/PC/Excercises/Ej1BuclesAnidados.java
@@ -0,0 +1,18 @@
+package org.example.PC.Excercises;
+
+import java.util.Scanner;
+
+public class Ej1BuclesAnidados {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Dime un entero positivo");
+        int numb= scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 1; i<= numb; i++) {
+            for (int j=numb-(numb-i); j<=numb+(i-1); j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
